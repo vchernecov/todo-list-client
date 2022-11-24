e7ac069107179134b41bbee078217e7075475e93
Viktor Chernetsov <v.chernecov@e2e4online.ru>
2022-11-24 17:55:06 +0300

    Init commit

---
+++ src/main/java/com/client/todo/TodoClientApplication.java
@@ -0,0 +1,13 @@
+package com.client.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoClientApplication.class, args);
+    }
+
+}
